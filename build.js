31917b238115696b80e2c6e84ddf78f26940f2e7
jonathan.rubin <jonathan.rubin@mailchimp.com>
2022-01-17 12:52:54 -0500

    added comments and watch

--- build.js
+++ build.js
@@ -5,5 +5,17 @@ require('esbuild').build({
   loader: {
     '.js': 'jsx'
   },
+  // will want to remove for production builds
   sourcemap: true,
+  // TODO: create separate prod and dev build files
+  // will want to add for production builds
+  // minify: true,
+  watch: {
+    onRebuild(error, result) {
+      if (error) console.error('watch build failed:', error)
+      else console.log('watch build succeeded:', result)
+    },
+  },
+}).then(result => {
+    console.log('watching...', result)
 }).catch(() => process.exit(1))
